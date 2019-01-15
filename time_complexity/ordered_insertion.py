1caadac6e5c4fec94e3be43ad05e3c263e6d32cf
GudniNatan <1493259+GudniNatan@users.noreply.github.com>
2019-01-15 15:56:08 +0000

    There is some problem with the binary search

--- time_complexity/ordered_insertion.py
+++ time_complexity/ordered_insertion.py
@@ -1,5 +1,6 @@
 from random import randint
 import timeit
+from insertion import random_number_insertion
 
 
 def insert(a_list: list, number: int, index: int):
@@ -19,15 +20,13 @@ def linear_search(a_list: list, number: int):
 def binary_search(a_list: list, number: int):
     low = 0
     high = len(a_list) - 1
-    while low <= high:
+    while low < high:
         mid = (low + high) // 2
         if a_list[mid] < number:
             low = mid + 1
-        elif a_list[mid] > number:
-            high = mid - 1
         else:
-            return mid
-    return len(a_list)
+            high = mid
+    return low
 
 
 def linear_insert(a_list: list, number: int):
@@ -43,17 +42,26 @@ def binary_insert(a_list: list, number: int):
     insert(a_list, number, index)
 
 
-def test_insert(n, insert_function):
+def test_insert(insert_function, a_list):
     '''Populates sorted list, complexity: O(n^2)'''
     my_list = list()
-    for _ in range(n):
-        insert_function(my_list, randint(1, 6))
+    for num in a_list:
+        insert_function(my_list, num)
+    return my_list
 
 if __name__ == "__main__":
+    testlist = random_number_insertion(1000)
+
     start_time = timeit.default_timer()
-    test_insert(100000, linear_insert)
+    sorted_list_1 = test_insert(linear_insert, testlist)
     print(timeit.default_timer() - start_time)
 
     start_time = timeit.default_timer()
-    test_insert(100000, binary_insert)
+    sorted_list_2 = test_insert(binary_insert, testlist)
     print(timeit.default_timer() - start_time)
+
+    for i, item in enumerate(zip(sorted_list_1, sorted_list_2)):
+        if item[0] != item[1]:
+            print(i)
+
+    assert sorted_list_1 == sorted_list_2
